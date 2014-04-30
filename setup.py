ba96a9d811378f428e15e786e8fbce8e48c50373
Francesco Salamida <salamida.francesco@gmail.com>
2014-04-30 15:32:34 +0200

    inject version and release into the doc

--- setup.py
+++ setup.py
@@ -51,10 +51,14 @@ class DocCommand(Command):
         self.mkpath(self.doctree_dir)
         self.builder_target_dir = 'doc'
         self.mkpath(self.builder_target_dir)
+        confoverrides = {
+                            'version': versioneer.get_version(),
+                            'release': versioneer.get_version(),
+                        }
 
         app = Sphinx(self.source_dir, self.source_dir,
                      self.build_dir, self.doctree_dir,
-                     'latex', {}, sys.stdout,
+                     'latex', confoverrides, sys.stdout,
                      freshenv=False)
 
         try:
@@ -74,7 +78,7 @@ class DocCommand(Command):
 
         app = Sphinx(self.source_dir, self.source_dir,
                      'doc', self.doctree_dir,
-                     'html', {}, sys.stdout,
+                     'html', confoverrides, sys.stdout,
                      freshenv=True)
 
         try:

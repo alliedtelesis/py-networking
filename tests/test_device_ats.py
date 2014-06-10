81bb4330c9ef3ecef77aadd5eff566e2f3575eb2
michele.mensuali <michele.mensuali@gmail.com>
2014-06-10 12:05:07 +0200

    ATS test is completely automatic

--- tests/test_device_ats.py
+++ tests/test_device_ats.py
@@ -33,104 +33,68 @@ Serial number:
 
 def test_save_config(dut, log_level):
     setup_dut(dut)
-    config_running = """
-!
-service password-encryption
-!
-no banner motd
-!
-username manager privilege 15 password 8 $1$bJoVec4D$JwOJGPr7YqoExA0GVasdE0
-!
-ssh server allow-users manager
-service ssh
-!
-service telnet
-!
-service http
-!
-no clock timezone
-!
-snmp-server
-!
-aaa authentication enable default local
-aaa authentication login default local
-!
-stack virtual-chassis-id 1726
-!
-ip domain-lookup
-!
-no service dhcp-server
-!
-no ip multicast-routing
-!
-spanning-tree mode rstp
-!
-switch 1 provision x600-48
-!
-interface port1.0.1-1.0.50
- switchport
- switchport mode access
-!
-interface vlan1
- ip address 10.17.39.254/24
-!
-line con 0
-line vty 0 4
-!
-end
+    config_no_vlan = """
+interface range ethernet 1/e(1-16)
+spanning-tree portfast
+exit
+vlan database
+vlan 2,10,30,100,1000,2000,3000,4000,4045,4093
+exit
+interface vlan 10
+name "long vlan name"
+exit
+interface vlan 1
+ip address 10.17.39.252 255.255.255.0
+name default_vlan
+exit
+hostname nac_dev
+ip ssh server
 """
-    config_startup="""
-!
-service password-encryption
-!
-no banner motd
-!
-username manager privilege 15 password 8 $1$bJoVec4D$JwOJGPr7YqoExA0GVasdE0
-!
-ssh server allow-users manager
-service ssh
-!
-service telnet
-!
-service http
-!
-no clock timezone
-!
-snmp-server
-!
-aaa authentication enable default local
-aaa authentication login default local
-!
-stack virtual-chassis-id 1726
-!
-ip domain-lookup
-!
-no service dhcp-server
-!
-no ip multicast-routing
-!
-spanning-tree mode rstp
-!
-switch 1 provision x600-48
-!
-interface port1.0.1-1.0.50
- switchport
- switchport mode access
-!
-interface vlan1
- ip address 10.17.39.254/24
-!
-line con 0
-line vty 0 4
-!
-end
+    config_with_vlan="""
+interface range ethernet 1/e(1-16)
+spanning-tree portfast
+exit
+vlan database
+vlan 2,10,30,100,1000,2000,3000,3999,4000,4045,4093
+exit
+interface vlan 10
+name "long vlan name"
+exit
+interface vlan 1
+ip address 10.17.39.252 255.255.255.0
+name default_vlan
+exit
+hostname nac_dev
+ip ssh server
 """
-    dut.add_cmd({'cmd': 'write'              , 'state':0, 'action':'SET_STATE','args':[1]})
-    dut.add_cmd({'cmd': 'show running-config', 'state':1, 'action':'PRINT','args':[config_running]})
-    dut.add_cmd({'cmd': 'show startup-config', 'state':1, 'action':'PRINT','args':[config_startup]})
+    dut.add_cmd({'cmd': 'show running-config', 'state':0, 'action':'PRINT','args':[config_no_vlan]})
+    dut.add_cmd({'cmd': 'show startup-config', 'state':0, 'action':'PRINT','args':[config_no_vlan]})
+    dut.add_cmd({'cmd': 'vlan database'      , 'state':0, 'action':'SET_PROMPT','args':['(config-vlan)#']})
+    dut.add_cmd({'cmd': 'vlan database'      , 'state':0, 'action':'SET_STATE','args':[1]})
+    dut.add_cmd({'cmd': 'vlan 3999'          , 'state':1, 'action':'SET_STATE','args':[2]})
+    dut.add_cmd({'cmd': 'show running-config', 'state':2, 'action':'PRINT','args':[config_with_vlan]})
+    dut.add_cmd({'cmd': 'show startup-config', 'state':2, 'action':'PRINT','args':[config_no_vlan]})
+    dut.add_cmd({'cmd': 'write'              , 'state':2, 'action':'SET_STATE','args':[3]})
+    dut.add_cmd({'cmd': 'show running-config', 'state':3, 'action':'PRINT','args':[config_with_vlan]})
+    dut.add_cmd({'cmd': 'show startup-config', 'state':3, 'action':'PRINT','args':[config_with_vlan]})
+    dut.add_cmd({'cmd': 'vlan database'      , 'state':3, 'action':'SET_PROMPT','args':['(config-vlan)#']})
+    dut.add_cmd({'cmd': 'vlan database'      , 'state':3, 'action':'SET_STATE','args':[4]})
+    dut.add_cmd({'cmd': 'no vlan 3999'       , 'state':4, 'action':'SET_STATE','args':[5]})
+    dut.add_cmd({'cmd': 'show running-config', 'state':5, 'action':'PRINT','args':[config_no_vlan]})
+    dut.add_cmd({'cmd': 'show startup-config', 'state':5, 'action':'PRINT','args':[config_with_vlan]})
+    dut.add_cmd({'cmd': 'write'              , 'state':5, 'action':'SET_STATE','args':[6]})
+    dut.add_cmd({'cmd': 'show running-config', 'state':6, 'action':'PRINT','args':[config_no_vlan]})
+    dut.add_cmd({'cmd': 'show startup-config', 'state':6, 'action':'PRINT','args':[config_no_vlan]})
 
     d=Device(host=dut.host,port=dut.port,protocol=dut.protocol,log_level=log_level)
     d.open()
+    assert d.config == d.system.get_startup_config()
+    d.vlan.create(3999)
+    assert d.config != d.system.get_startup_config()
+    d.system.save_config()
+    assert d.config == d.system.get_startup_config()
+    d.vlan.delete(3999)
+    assert d.config != d.system.get_startup_config()
     d.system.save_config()
     assert d.config == d.system.get_startup_config()
     d.close()

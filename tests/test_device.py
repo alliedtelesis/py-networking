61d28813b6d61730d6c3cd53949f16bb53d37f81
michele.mensuali <michele.mensuali@gmail.com>
2014-06-10 11:48:21 +0200

    AWP test is completely automatic

--- tests/test_device.py
+++ tests/test_device.py
@@ -185,7 +185,7 @@ def test_system(dut, log_level):
 
 def test_save_config(dut, log_level):
     setup_dut(dut)
-    config_running = """
+    config_no_vlan = """
 !
 service password-encryption
 !
@@ -231,7 +231,7 @@ line vty 0 4
 !
 end
 """
-    config_startup="""
+    config_with_vlan="""
 !
 service password-encryption
 !
@@ -272,17 +272,42 @@ interface port1.0.1-1.0.50
 interface vlan1
  ip address 10.17.39.254/24
 !
+vlan database
+ vlan 3999 state enable
+!
 line con 0
 line vty 0 4
 !
 end
 """
-    dut.add_cmd({'cmd': 'write'              , 'state':0, 'action':'SET_STATE','args':[1]})
-    dut.add_cmd({'cmd': 'show running-config', 'state':1, 'action':'PRINT','args':[config_running]})
-    dut.add_cmd({'cmd': 'show startup-config', 'state':1, 'action':'PRINT','args':[config_startup]})
+    dut.add_cmd({'cmd': 'show running-config', 'state':0, 'action':'PRINT','args':[config_no_vlan]})
+    dut.add_cmd({'cmd': 'show startup-config', 'state':0, 'action':'PRINT','args':[config_no_vlan]})
+    dut.add_cmd({'cmd': 'vlan database'      , 'state':0, 'action':'SET_PROMPT','args':['(config-vlan)#']})
+    dut.add_cmd({'cmd': 'vlan database'      , 'state':0, 'action':'SET_STATE','args':[1]})
+    dut.add_cmd({'cmd': 'vlan 3999'          , 'state':1, 'action':'SET_STATE','args':[2]})
+    dut.add_cmd({'cmd': 'show running-config', 'state':2, 'action':'PRINT','args':[config_with_vlan]})
+    dut.add_cmd({'cmd': 'show startup-config', 'state':2, 'action':'PRINT','args':[config_no_vlan]})
+    dut.add_cmd({'cmd': 'write'              , 'state':2, 'action':'SET_STATE','args':[3]})
+    dut.add_cmd({'cmd': 'show running-config', 'state':3, 'action':'PRINT','args':[config_with_vlan]})
+    dut.add_cmd({'cmd': 'show startup-config', 'state':3, 'action':'PRINT','args':[config_with_vlan]})
+    dut.add_cmd({'cmd': 'vlan database'      , 'state':3, 'action':'SET_PROMPT','args':['(config-vlan)#']})
+    dut.add_cmd({'cmd': 'vlan database'      , 'state':3, 'action':'SET_STATE','args':[4]})
+    dut.add_cmd({'cmd': 'no vlan 3999'       , 'state':4, 'action':'SET_STATE','args':[5]})
+    dut.add_cmd({'cmd': 'show running-config', 'state':5, 'action':'PRINT','args':[config_no_vlan]})
+    dut.add_cmd({'cmd': 'show startup-config', 'state':5, 'action':'PRINT','args':[config_with_vlan]})
+    dut.add_cmd({'cmd': 'write'              , 'state':5, 'action':'SET_STATE','args':[6]})
+    dut.add_cmd({'cmd': 'show running-config', 'state':6, 'action':'PRINT','args':[config_no_vlan]})
+    dut.add_cmd({'cmd': 'show startup-config', 'state':6, 'action':'PRINT','args':[config_no_vlan]})
 
     d=Device(host=dut.host,port=dut.port,protocol=dut.protocol,log_level=log_level)
     d.open()
+    assert d.config == d.system.get_startup_config()
+    d.vlan.create(3999)
+    assert d.config != d.system.get_startup_config()
+    d.system.save_config()
+    assert d.config == d.system.get_startup_config()
+    d.vlan.delete(3999)
+    assert d.config != d.system.get_startup_config()
     d.system.save_config()
     assert d.config == d.system.get_startup_config()
     d.close()

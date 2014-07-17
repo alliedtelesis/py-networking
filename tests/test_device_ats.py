bb8c826fefb18a75ba0f55c1de19fcd880480dcb
michele.mensuali <michele.mensuali@gmail.com>
2014-07-17 14:39:13 +0200

    Improved tests

--- tests/test_device_ats.py
+++ tests/test_device_ats.py
@@ -12,29 +12,6 @@ from paramiko.rsakey import RSAKey
 from pprint import pprint
 
 
-def tftp_server_for_ever(port):
-    tftp_client_dir = './tftp_client_dir'
-    if (os.path.exists(tftp_client_dir) == False):
-        os.mkdir(tftp_client_dir)
-    tftp_server_dir = './tftp_server_dir'
-    if (os.path.exists(tftp_server_dir) == False):
-        os.mkdir(tftp_server_dir)
-        ip_address = socket.gethostbyname(socket.getfqdn())
-        server = tftpy.TftpServer(tftp_server_dir)
-        server.listen(ip_address, port)
-
-
-def setup_tftp_server(dut):
-    if (dut.mode != 'emulated'):
-        assert 'root' == getpass.getuser()
-    dut.tftp_port = 69
-    if (getpass.getuser() != 'root'):
-        dut.tftp_port = 20069
-    dut.tftp_server_thread = threading.Thread(target=tftp_server_for_ever, args=(dut.tftp_port,))
-    dut.tftp_server_thread.daemon = True
-    dut.tftp_server_thread.start()
-
-
 def setup_dut(dut):
     dut.reset()
     dut.prompt = '#'
@@ -59,6 +36,33 @@ Unit     Up time
 Unit Number:   1
 Serial number:
     """]})
+
+
+def tftp_server_for_ever(port):
+    tftp_client_dir = './tftp_client_dir'
+    if (os.path.exists(tftp_client_dir) == False):
+        os.mkdir(tftp_client_dir)
+    tftp_server_dir = './tftp_server_dir'
+    if (os.path.exists(tftp_server_dir) == False):
+        os.mkdir(tftp_server_dir)
+        ip_address = socket.gethostbyname(socket.getfqdn())
+        server = tftpy.TftpServer(tftp_server_dir)
+        server.listen(ip_address, port)
+
+
+def setup_tftp_server(dut, image_name):
+    dut.tftp_port = 69
+    if (dut.mode == 'emulated'):
+        if (getpass.getuser() != 'root'):
+            dut.tftp_port = 20069
+        myfile = open(image_name, 'w')
+        myfile.write('1')
+        myfile.close()
+    else:
+        assert 'root' == getpass.getuser()
+    dut.tftp_server_thread = threading.Thread(target=tftp_server_for_ever, args=(dut.tftp_port,))
+    dut.tftp_server_thread.daemon = True
+    dut.tftp_server_thread.start()
 
 
 def test_save_config(dut, log_level):
@@ -159,8 +163,10 @@ Unit  Image  Filename   Version    Date                    Status
 "*" designates that the image was selected for the next boot
 
 """]
+    image_name = '8000s-5.4.3-3.9.rel'
+    false_image_name = '8001s-5.4.3-3.9.rel'
     setup_dut(dut)
-    setup_tftp_server(dut)
+    setup_tftp_server(dut, image_name)
     local_tftp_server = socket.gethostbyname(socket.getfqdn())
     update_cmd = 'copy tftp://{0}/test_file_2.cfg image'.format(local_tftp_server)
     dut.add_cmd({'cmd': 'show bootvar', 'state':0, 'action':'PRINT','args': output_0})
@@ -168,17 +174,16 @@ Unit  Image  Filename   Version    Date                    Status
     dut.add_cmd({'cmd': 'show bootvar', 'state':1, 'action':'PRINT','args': output_1})
     d=Device(host=dut.host,port=dut.port,protocol=dut.protocol,log_level=log_level)
     d.open()
-    assert (os.path.exists('8000s-5.4.3-3.9.rel') == True)
-    assert (os.path.exists('8001s-5.4.3-3.9.rel') == False)
+    assert (os.path.exists(image_name) == True)
+    assert (os.path.exists(false_image_name) == False)
     with pytest.raises(KeyError) as excinfo:
-        d.system.update(name='8001s-5.4.3-3.9.rel', port=dut.tftp_port)
-    d.system.update(name='8000s-5.4.3-3.9.rel', port=dut.tftp_port)
+        d.system.update(name=false_image_name, port=dut.tftp_port)
+    d.system.update(name=image_name, port=dut.tftp_port)
     d.close()
 
     if (dut.mode == 'emulated'):
         os.remove('tftp_client_dir/image')
         os.rmdir('tftp_client_dir')
-        os.remove('tftp_server_dir/8000s-5.4.3-3.9.rel')
+        os.remove('tftp_server_dir/' + image_name)
         os.rmdir('tftp_server_dir')
-
-
+        os.remove(image_name)

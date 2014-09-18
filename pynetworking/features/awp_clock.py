aa453c1e640a24a33269f5363e486f9a640ce4d0
michele.mensuali <michele.mensuali@gmail.com>
2014-09-18 15:45:20 +0200

    Fixed week 5 bug

--- pynetworking/features/awp_clock.py
+++ pynetworking/features/awp_clock.py
@@ -30,7 +30,7 @@ class awp_clock(Feature):
         self._d.log_info("update")
 
         if (dt == None and tz == None):
-            raise KeyError('either dt or timezone argument must be given')
+            raise KeyError('either datetime or timezone argument must be given')
 
         if (dt != None):
             # set date and time
@@ -80,141 +80,6 @@ class awp_clock(Feature):
                 ew = str((int(end_dst.strftime('%d')) - 1)/7 + 1)
                 em = end_dst.strftime('%b')
 
-                # # DST begin rules
-                # go_in_the_day_before = False
-                # go_in_the_day_after = False
-                #
-                # mm = begin_dst.strftime('%M')
-                # hh = int(begin_dst.strftime('%H'))
-                #
-                # if sign == '-':
-                #     if (hh - int(off_h) < 0):
-                #         go_in_the_day_before = True
-                # else:
-                #     if (hh + int(off_h) >= 24):
-                #         go_in_the_day_after = True
-                #
-                # if sign == '-':
-                #     if (go_in_the_day_before == False):
-                #          hh = hh - int(off_h)
-                #     else:
-                #          hh = hh + 24 - int(off_h)
-                # else:
-                #     if (go_in_the_day_after == False):
-                #         hh = hh + int(off_h)
-                #     else:
-                #         hh = hh + int(off_h) - 24
-                # bt = "{0}:{1}".format(hh,mm)
-                #
-                # dd = int(begin_dst.strftime('%w'))
-                # ww = int(begin_dst.strftime('%d'))
-                # if sign == '-':
-                #     if (go_in_the_day_before == True):
-                #          if (dd - 1 < 0):
-                #              dd = 6
-                #              ww = ww - 1
-                #          else:
-                #              dd = dd - 1
-                # else:
-                #     if (go_in_the_day_after == True):
-                #          if (dd + 1 > 6):
-                #              dd = 0
-                #              ww = ww + 1
-                #          else:
-                #              dd = dd + 1
-                # bd = str(dd)
-                # bw = str(ww)
-                #
-                # bm = begin_dst.strftime('%b')
-                #
-                # # DST end rules
-                # go_in_the_day_before = False
-                # go_in_the_day_after = False
-                #
-                # mm = end_dst.strftime('%M')
-                # hh = int(end_dst.strftime('%H'))
-                #
-                # if sign == '-':
-                #     if (hh - int(off_h) < 0):
-                #         go_in_the_day_before = True
-                # else:
-                #     if (hh + int(off_h) >= 24):
-                #         go_in_the_day_after = True
-                #
-                # if sign == '-':
-                #     if (go_in_the_day_before == False):
-                #          hh = hh - int(off_h)
-                #     else:
-                #          hh = hh + 24 - int(off_h)
-                # else:
-                #     if (go_in_the_day_after == False):
-                #         hh = hh + int(off_h)
-                #     else:
-                #         hh = hh + int(off_h) - 24
-                # et = "{0}:{1}".format(hh,mm)
-                #
-                # em = end_dst.strftime('%b')
-                #
-                # dd = int(end_dst.strftime('%a'))
-                # ww = int(end_dst.strftime('%d'))
-                # if sign == '-':
-                #     if (go_in_the_day_before == True):
-                #          if (dd - 1 < 0):
-                #              dd = 6
-                #              ww = ww - 1
-                #          else:
-                #              dd = dd - 1
-                # else:
-                #     if (go_in_the_day_after == True):
-                #          if (dd + 1 > 6):
-                #              dd = 0
-                #              ww = ww + 1
-                #          else:
-                #              dd = dd + 1
-                # ed = str(dd)
-                # ew = str(ww)
-
-                # bw = str((int(begin_dst.strftime('%d')) - 1)/7 + 1)
-                # bd = begin_dst.strftime('%a')
-                # bm = begin_dst.strftime('%b')
-                # hh = int(begin_dst.strftime('%H'))
-                # mm = begin_dst.strftime('%M')
-                # if sign == '-':
-                #     if (hh + int(off_h) >= 0):
-                #         hh -= int(off_h)
-                #     else:
-                #         hh = hh + 24 - int(off_h)
-                # else:
-                #     if (hh + int(off_h) < 24):
-                #         hh += int(off_h)
-                #     else:
-                #         hh = hh + int(off_h) - 24
-                #         if (int(begin_dst.strftime('%d')) - 1)/7 < 0:
-                #             bw = '6'
-                #             bd = str(int(bd)-1)
-                #         else:
-                #             bw = str((int(begin_dst.strftime('%d')) - 1)/7)
-                # bt = "{0}:{1}".format(hh,mm)
-                #
-                # ew = str((int(end_dst.strftime('%d')) - 1)/7 + 1)
-                # ed = end_dst.strftime('%a')
-                # em = end_dst.strftime('%b')
-                # hh = int(end_dst.strftime('%H'))
-                # mm = end_dst.strftime('%M')
-                # if sign == '-':
-                #     hh -= int(off_h) + 1
-                # else:
-                #     if (hh + int(off_h) + 1 < 24):
-                #         hh += int(off_h) + 1
-                #     else:
-                #         hh = hh + int(off_h) + 1 - 24
-                #         if (int(begin_dst.strftime('%d')) - 1)/7 + 1 < 6:
-                #             bw = str((int(begin_dst.strftime('%d')) - 1)/7 + 2)
-                #         else:
-                #             bw = '0'
-                #             bd = str(int(bd)+1)
-                #
-                # et = "{0}:{1}".format(hh,mm)
                 om = '60'
 
                 self._d.log_info("Setting timezone {0} with {1} {2} {3} {4} {5} {6} {7} {8} {9}".format(tz_name, bw, bd, bm, bt, ew, ed, em, et, om))
@@ -242,19 +107,16 @@ class awp_clock(Feature):
         tt = tz._utc_transition_times
         offset = dt.hour
         year = dt.year
-        self._d.log_info("offset {0} year {1}".format(offset, year))
 
         for index in range(len(tt)):
-            if tt[index].year == year:
-                # self._d.log_info("index is {0}".format(index))
-                # self._d.log_info("begin with {0}".format(tt[index]))
+            if tt[index].year >= year:
+                # search the maximum day value so to detect whether or not the day is in week 5
                 utc = pytz.utc
                 utc_dt = datetime(tt[index].year, tt[index].month, tt[index].day, tt[index].hour + 1, tt[index].minute + 1, tzinfo = utc)
                 temp_dt = utc_dt.astimezone(tz)
-                if temp_dt.dst() == timedelta(0):
+                if temp_dt.dst() == timedelta(0) or tt[index+2].day > tt[index].day or tt[index-2].day > tt[index].day:
                     continue
-                self._d.log_info("index is {0}".format(index))
-                self._d.log_info("begin with {0}".format(tt[index]))
+                self._d.log_debug("DST start is {0} (index {1})".format(tt[index], index))
                 a_tt = datetime(tt[index].year, tt[index].month, tt[index].day, tt[index].hour, tt[index].minute, tzinfo = utc)
                 ret_tt = a_tt.astimezone(tz)
                 return ret_tt
@@ -266,19 +128,18 @@ class awp_clock(Feature):
         tt = tz._utc_transition_times
         offset = dt.hour
         year = dt.year
-        self._d.log_info("offset {0} year {1} {2}".format(offset, year, len(tt)))
 
         for index in range(len(tt)):
-            if tt[index].year == year:
-                # self._d.log_info("index is {0}".format(index))
-                # self._d.log_info("end with {0}".format(tt[index]))
+            if tt[index].year >= year:
+                # search the maximum day value so to detect whether or not the day is in week 5
                 utc = pytz.utc
                 utc_dt = datetime(tt[index].year, tt[index].month, tt[index].day, tt[index].hour + 1, tt[index].minute + 1, tzinfo = utc)
                 temp_dt = utc_dt.astimezone(tz)
-                if temp_dt.dst() != timedelta(0):
+                if temp_dt.dst() != timedelta(0) or tt[index+2].day > tt[index].day or tt[index-2].day > tt[index].day:
                     continue
-                self._d.log_info("index is {0}".format(index))
-                self._d.log_info("end with {0}".format(tt[index]))
+                self._d.log_debug("DST end is {0} (index {1})".format(tt[index], index))
+
+                # adjustment due to DST time
                 a_tt = datetime(tt[index].year, tt[index].month, tt[index].day, tt[index].hour + 1, tt[index].minute, tzinfo = utc)
                 ret_tt = a_tt.astimezone(tz)
                 return ret_tt

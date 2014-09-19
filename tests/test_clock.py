1f06103b9678a3e57094af4a70de6ca3dbd78416
michele.mensuali <michele.mensuali@gmail.com>
2014-09-19 10:42:31 +0200

    Commands sent to device and fixed dictionary

--- tests/test_clock.py
+++ tests/test_clock.py
@@ -48,20 +48,32 @@ Summer time recurring: Yes
     tz4 = timezone('America/New_York')
 
     dut.add_cmd({'cmd': 'show clock'                , 'state':0, 'action':'PRINT','args': clock_output_0})
-    dut.add_cmd({'cmd': 'clock timezone EDT minus 4', 'state':0, 'action':'SET_STATE','args': [1]})
-    dut.add_cmd({'cmd': 'clock summer-time EDT recurring 2 Sun Mar 2:00 1 Sun Nov 2:00 60', 'state':1, 'action':'SET_STATE','args': [2]})
+    dut.add_cmd({'cmd': 'clock timezone CEST plus 2', 'state':0, 'action':'SET_STATE','args': [1]})
+    dut.add_cmd({'cmd': 'clock summer-time CEST recurring 5 Sun Mar 2:00 5 Sun Oct 3:00 60', 'state':1, 'action':'SET_STATE','args': [2]})
     dut.add_cmd({'cmd': 'show clock'                , 'state':2, 'action':'PRINT','args': clock_output_1})
+    dut.add_cmd({'cmd': 'clock timezone EDT minus 4', 'state':2, 'action':'SET_STATE','args': [3]})
+    dut.add_cmd({'cmd': 'clock summer-time EDT recurring 2 Sun Mar 2:00 1 Sun Nov 2:00 60', 'state':3, 'action':'SET_STATE','args': [4]})
+    dut.add_cmd({'cmd': 'show clock'                , 'state':4, 'action':'PRINT','args': clock_output_1})
     d=Device(host=dut.host,port=dut.port,protocol=dut.protocol, log_level=log_level)
     d.open()
     # print(dut.port)
     # sleep(1000)
     with pytest.raises(KeyError) as excinfo:
         d.clock.update(dt=None, tz=None)
-    # d.clock.update(tz=tz1)
+    d.clock.update(tz=tz1)
+    assert d.clock['timezone_name'] == 'CEST'
+    assert d.clock['timezone_offset'] == '+01:00'
+    assert d.clock['summertime_start'] == 'Last Sunday in March at 02:00:00'
+    assert d.clock['summertime_end'] == 'Last Sunday in October at 03:00:00'
+    assert d.clock['summertime_offset'] == '60'
     # d.clock.update(tz=tz2)
     # d.clock.update(tz=tz3)
-    d.clock.update(tz=tz4)
-    # d.clock.update(dt=dt)
 
+    d.clock.update(tz=tz4)
     assert d.clock['timezone_name'] == 'EDT'
+    assert d.clock['timezone_offset'] == '-05:00'
+    assert d.clock['summertime_start'] == 'Second Sunday in March at 02:00:00'
+    assert d.clock['summertime_end'] == 'First Sunday in November at 02:00:00'
+    assert d.clock['summertime_offset'] == '60'
+
     d.close()

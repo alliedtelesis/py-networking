6d2e89fb45adf1ea29ca704e7323b6bf54de6fe8
michele.mensuali <michele.mensuali@gmail.com>
2014-09-25 15:04:10 +0200

    Added basic infrastructure

---
+++ pynetworking/features/awp_ntp.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+from pynetworking import Feature
+from pprint import pformat
+from time import sleep
+import re
+import json
+try:
+    from collections import OrderedDict
+except ImportError: #pragma: no cover
+    from ordereddict import OrderedDict
+
+
+class awp_ntp(Feature):
+    """
+    ntp feature implementation for AWP
+    """
+    def __init__(self, device, **kvargs):
+        Feature.__init__(self, device, **kvargs)
+        self._ntp={}
+        self._d = device
+        self._d.log_debug("loading feature")
+
+
+    def load_config(self, config):
+        self._d.log_info("loading config")
+        self._ntp = OrderedDict()
+
+        #ntp address-table static xxxx.xxxx.xxxx forward interface port1.0.1 vlan 1
+        ifre = re.compile('ntp\s+address-table\s+static\s+(?P<ntp>[^\s]+)\s+'
+                          '(?P<action>[^\s]+)\s+'
+                          'interface\s+(?P<interface>[^\s]+)\s+'
+                          'vlan\s+(?P<vlan>\d+)')
+
+        for line in config.split('\n'):
+            self._d.log_debug("line is {0}".format(line))
+            m = ifre.match(line)
+            if m:
+                key = m.group('ntp')
+                self._ntp[key] = {'vlan': m.group('vlan'),
+                                  'interface': m.group('interface'),
+                                  'action': m.group('action'),
+                                  'type': 'static'
+                                 }
+        self._d.log_info(self._ntp)
+
+
+    def create(self, address):
+        self._d.log_info("add NTP server {0}".format(address))
+        self._update_ntp()
+
+        if address in self._ntp.keys():
+            raise KeyError('NTP server {0} already added'.format(address))
+
+        set_cmd = 'ntp peer {0}'.format(address)
+        cmds = {'cmds': [{'cmd': 'enable', 'prompt': '\#'},
+                         {'cmd': 'conf t', 'prompt': '\(config\)\#'},
+                         {'cmd': set_cmd , 'prompt': '\(config\)\#'},
+                         {'cmd': chr(26) , 'prompt': '\#'}
+                        ]}
+        self._device.cmd(cmds, cache=False, flush_cache=True)
+        self._update_ntp()
+
+
+    # def update(self, ntp, interface, forward=True, vlan=1):
+    #     self._d.log_info("update MAC address {0} entry".format(ntp))
+    #     self._update_mac()
+    #
+    #     ntp = self._get_dotted_mac(ntp)
+    #     if ntp not in self._ntp.keys():
+    #         raise KeyError('MAC address {0} is not existing'.format(ntp))
+    #
+    #     fwd = 'forward'
+    #     if (forward == False):
+    #        fwd = 'discard'
+    #     set_cmd = 'ntp address-table static {0} {1} interface {2} vlan {3}'.format(ntp, fwd, interface, vlan)
+    #     cmds = {'cmds': [{'cmd': 'enable', 'prompt': '\#'},
+    #                      {'cmd': 'conf t', 'prompt': '\(config\)\#'},
+    #                      {'cmd': set_cmd , 'prompt': '\(config\)\#'},
+    #                      {'cmd': chr(26) , 'prompt': '\#'}
+    #                     ]}
+    #     self._device.cmd(cmds, cache=False, flush_cache=True)
+    #     sleep(1)
+    #     self._update_mac()
+
+
+    def delete(self, address=''):
+        self._d.log_info("remove NTP server {0}".format(address))
+        self._update_ntp()
+
+        if address != '' and address not in self._ntp.keys():
+            raise KeyError('NTP server {0} not present'.format(address))
+
+        del_cmd = 'no ntp peer {0}'.format(address)
+        cmds = {'cmds': [{'cmd': 'enable', 'prompt': '\#'},
+                         {'cmd': 'conf t', 'prompt': '\(config\)\#'},
+                         {'cmd': del_cmd , 'prompt': '\(config\)\#'},
+                         {'cmd': chr(26) , 'prompt': '\#'}
+                        ]}
+        self._device.cmd(cmds, cache=False, flush_cache=True)
+        self._update_ntp()
+
+
+    def items(self):
+        self._update_ntp()
+        return self._ntp.items()
+
+
+    def keys(self):
+        self._update_ntp()
+        return self._ntp.keys()
+
+
+    def __getitem__(self, address):
+        self._update_ntp()
+        if address not in self._ntp.keys():
+            raise KeyError('NTP server {0} does not exist'.format(address))
+        return self._ntp[address]
+
+
+    def _update_ntp(self):
+        self._d.log_info("_update_ntp")
+        self._ntp = OrderedDict()
+
+        # 1    port1.0.1    0000.cd1d.7eb0   forward   dynamic
+        ifre = re.compile('(?P<address>[^\s]+)\s+'
+                          '(?P<polltime>[^\s]+)\s+'
+                          '(?P<status>[^\s]+)')
+        self._device.cmd("terminal length 0")
+        for line in self._device.cmd("show ntp status").split('\n'):
+            self._d.log_debug("line is {0}".format(line))
+            m = ifre.match(line)
+            if m:
+                key = m.group('address')
+                self._ntp[key] = {'polltime': m.group('polltime'),
+                                  'status': m.group('status')
+                                 }
+        self._d.log_debug("ntp {0}".format(pformat(json.dumps(self._ntp))))
---
+++ tests/test_ntp_ats.py
@@ -0,0 +1,74 @@
+import pytest
+from pynetworking import Device
+from time import sleep
+from paramiko.rsakey import RSAKey
+
+
+def setup_dut(dut):
+    dut.reset()
+    dut.add_cmd({'cmd':'show version',        'state':-1, 'action': 'PRINT','args':["""
+AlliedWare Plus (TM) 5.4.2 09/25/13 12:57:26
+
+Build name : x600-5.4.2-3.14.rel
+Build date : Wed Sep 25 12:57:26 NZST 2013
+Build type : RELEASE
+    """]})
+
+
+def test_ntp_crud(dut, log_level):
+    output_0 = ["""
+"""]
+    output_1 = ["""
+"""]
+    output_2 = ["""
+"""]
+    output_3 = ["""
+"""]
+    setup_dut(dut)
+
+    ntp1_address = 'ntp.inrim.it'
+    ntp2_address = '10.17.39.111'
+    bad_ntp_address = '10.10.10.10.10'
+    create_cmd_1 = 'ntp peer {0}'.format(ntp1_address)
+    create_cmd_2 = 'ntp peer {0}'.format(ntp2_address)
+    delete_cmd_1 = 'no ntp peer {0}'.format(ntp1_address)
+    delete_cmd_2 = 'no ntp peer'
+
+    dut.add_cmd({'cmd': 'show ntp status', 'state':0, 'action':'PRINT'    ,'args': output_0})
+    dut.add_cmd({'cmd': create_cmd_1     , 'state':0, 'action':'SET_STATE','args':[1]})
+    dut.add_cmd({'cmd': 'show ntp status', 'state':1, 'action':'PRINT'    ,'args': output_1})
+    dut.add_cmd({'cmd': create_cmd_2     , 'state':1, 'action':'SET_STATE','args':[2]})
+    dut.add_cmd({'cmd': 'show ntp status', 'state':2, 'action':'PRINT'    ,'args': output_2})
+    dut.add_cmd({'cmd': delete_cmd_1     , 'state':2, 'action':'SET_STATE','args':[3]})
+    dut.add_cmd({'cmd': 'show ntp status', 'state':3, 'action':'PRINT'    ,'args': output_3})
+    dut.add_cmd({'cmd': delete_cmd_2     , 'state':3, 'action':'SET_STATE','args':[4]})
+    dut.add_cmd({'cmd': 'show ntp status', 'state':4, 'action':'PRINT'    ,'args': output_0})
+
+    d=Device(host=dut.host,port=dut.port,protocol=dut.protocol, log_level=log_level)
+    d.open()
+    with pytest.raises(KeyError) as excinfo:
+        d.ntp[bad_ntp_address]
+
+    assert ntp1_address not in d.ntp.keys()
+    d.ntp.create(ntp1_address)
+    with pytest.raises(KeyError) as excinfo:
+        d.ntp.create(ntp1_address)
+    assert ntp1_address in d.ntp.keys()
+    assert ntp2_address not in d.ntp.keys()
+    d.ntp.create(ntp2_address, 120)
+    assert ntp2_address in d.ntp.keys()
+
+    with pytest.raises(KeyError) as excinfo:
+        d.ntp.update(bad_ntp_address)
+    d.ntp.update(ntp1_address, 240)
+    assert ntp[ntp1_address][polltime] == 240
+    d.ntp.update(ntp2_address)
+    assert ntp[ntp2_address][polltime] == 60
+
+    with pytest.raises(KeyError) as excinfo:
+        d.ntp.delete(bad_ntp_address)
+    d.ntp.delete(ntp1_address)
+    assert ntp1_address not in d.ntp.keys()
+    d.ntp.delete()
+    assert ntp2_address not in d.ntp.keys()
+    d.close()

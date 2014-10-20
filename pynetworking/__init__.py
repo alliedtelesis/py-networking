deb517460cc95211438a90e4df67fbd25a044743
michele.mensuali <michele.mensuali@gmail.com>
2014-10-20 10:27:35 +0200

    Solved last pep8 inconsistency

--- pynetworking/__init__.py
+++ pynetworking/__init__.py
@@ -16,6 +16,9 @@ warnings.filterwarnings(
     DeprecationWarning
 )
 
+from pynetworking.Device import Device
+__username__ = Device.username
+
 import inspect
 __all__ = [name for name, obj in locals().items() if not (name.startswith('_') or inspect.ismodule(obj))]
 

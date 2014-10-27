a6848ed51edd0257bd1d65ca9d575555ec085020
michele.mensuali <michele.mensuali@gmail.com>
2014-10-27 10:41:32 +0100

    Added ntp tests with host name

--- tests/test_ntp.py
+++ tests/test_ntp.py
@@ -68,7 +68,6 @@ end
 
     ntp1_address = '193.204.114.233'
     ntp2_address = '193.204.114.105'
-    # ntp2_string_address = 'ntp.inrim.it'
     bad_ntp_address = '10.10.10.10.10'
     create_cmd_1 = 'ntp peer {0}'.format(ntp1_address)
     create_cmd_2 = 'ntp peer {0}'.format(ntp2_address)
@@ -112,4 +111,75 @@ end
     assert ntp2_address not in d.ntp.keys()
     d.ntp.delete(sleep_time=dut.sleep_time)
     assert ntp1_address not in d.ntp.keys()
+    d.close()
+
+def test_ntp_host_name(dut, log_level, use_mock):
+    # Add the routes manually to have the NTP servers reachable.
+    #
+    # ip route 0.0.0.0/0 10.17.39.1
+    #
+    # Note that NTP server addresses are shown in the numeric form, even if they have been set in the literal one.
+    output_0 = ["""
+  address          ref clock       st  when  poll reach   delay  offset    disp
+"""]
+    output_1 = ["""
+  address          ref clock       st  when  poll reach   delay  offset    disp
+*~193.204.114.105  CTD              1     2    64   001    21.8    11.2  7937.5
+"""]
+    output_rc_0 = ["""
+"""]
+    output_rc_1 = ["""
+ip name-server 10.17.39.11
+"""]
+    output_ping = ["""
+PING ntp.inrim.it (193.204.114.105) 56(84) bytes of data.
+64 bytes from ntp.ien.it (193.204.114.105): icmp_req=1 ttl=52 time=28.6 ms
+64 bytes from ntp.inrim.it (193.204.114.105): icmp_req=2 ttl=52 time=21.2 ms
+64 bytes from ntp.ien.it (193.204.114.105): icmp_req=3 ttl=52 time=24.7 ms
+64 bytes from ntp.inrim.it (193.204.114.105): icmp_req=4 ttl=52 time=22.6 ms
+64 bytes from ntp.ien.it (193.204.114.105): icmp_req=5 ttl=52 time=21.4 ms
+
+--- ntp.inrim.it ping statistics ---
+5 packets transmitted, 5 received, 0% packet loss, time 4005ms
+rtt min/avg/max/mdev = 21.289/23.755/28.617/2.729 ms
+"""]
+
+    setup_dut(dut)
+
+    name_server_primary = '10.17.39.11'
+    ntp_host_name = 'ntp.inrim.it'
+
+    adddns_cmd = 'ip name-server {0}'.format(name_server_primary)
+    deldns_cmd = 'no ip name-server {0}'.format(name_server_primary)
+    ping_cmd = 'ping {0}'.format(ntp_host_name)
+    create_cmd = 'ntp peer {0}'.format(ntp_host_name)
+    # delete_cmd = 'no ntp peer {0}'.format(ntp_host_name)
+    delete_cmd = 'no ntp peer 193.204.114.105'
+
+    dut.add_cmd({'cmd': 'show running-config', 'state': 0, 'action': 'PRINT', 'args': output_rc_0})
+    dut.add_cmd({'cmd': adddns_cmd, 'state': 0, 'action': 'SET_STATE', 'args': [1]})
+    dut.add_cmd({'cmd': 'show running-config', 'state': 1, 'action': 'PRINT', 'args': output_rc_1})
+    dut.add_cmd({'cmd': ping_cmd, 'state': 1, 'action': 'PRINT', 'args': output_ping})
+    dut.add_cmd({'cmd': 'show ntp associations', 'state': 1, 'action': 'PRINT', 'args': output_0})
+    dut.add_cmd({'cmd': create_cmd, 'state': 1, 'action': 'SET_STATE', 'args': [2]})
+    dut.add_cmd({'cmd': 'show ntp associations', 'state': 2, 'action': 'PRINT', 'args': output_1})
+    dut.add_cmd({'cmd': delete_cmd, 'state': 2, 'action': 'SET_STATE', 'args': [3]})
+    dut.add_cmd({'cmd': 'show ntp associations', 'state': 3, 'action': 'PRINT', 'args': output_0})
+    dut.add_cmd({'cmd': 'show running-config', 'state': 3, 'action': 'PRINT', 'args': output_rc_1})
+    dut.add_cmd({'cmd': deldns_cmd, 'state': 3, 'action': 'SET_STATE', 'args': [4]})
+    dut.add_cmd({'cmd': 'show running-config', 'state': 4, 'action': 'PRINT', 'args': output_rc_0})
+
+    d = Device(host=dut.host, port=dut.port, protocol=dut.protocol, log_level=log_level, mock=use_mock)
+    d.open()
+    d.dns.create(name_servers=name_server_primary)
+    ntp_ip_address = d.dns.read(ntp_host_name)
+
+    assert ntp_ip_address not in d.ntp.keys()
+    d.ntp.create(ntp_host_name, sleep_time=dut.sleep_time)
+    assert ntp_ip_address in d.ntp.keys()
+    # d.ntp.delete(ntp_host_name)
+    d.ntp.delete(ntp_ip_address)
+    assert ntp_ip_address not in d.ntp.keys()
+
+    d.dns.delete(name_servers=name_server_primary)
     d.close()
--- tests/test_ntp_ats.py
+++ tests/test_ntp_ats.py
@@ -234,3 +234,99 @@ Unicast servers:
     d.ntp.delete()
     assert ntp_address_1 not in d.ntp.keys()
     d.close()
+
+def test_ntp_host_name(dut, log_level, use_mock):
+    # Add the routes manually to have the NTP servers reachable.
+    #
+    # ip route 0.0.0.0/0 10.17.39.1
+    #
+    # Note that NTP server addresses are shown in the numeric form, even if they have been set in the literal one.
+    output_0 = ["""
+Clock is not synchronized
+
+Unicast servers:
+
+    Server       Status      Last Response     Offset   Delay
+                                               [mSec]   [mSec]
+--------------- --------- ------------------- --------- -------
+"""]
+    output_1 = ["""
+Clock is synchronized, stratum 1, reference is 193.204.114.105, unicast
+
+Unicast servers:
+
+    Server       Status      Last Response     Offset   Delay
+                                               [mSec]   [mSec]
+--------------- --------- ------------------- --------- -------
+193.204.114.105    up     09:44:04.0 UTC Sep    -9387      0
+                          30 2014
+"""]
+    output_rc_0 = ["""
+"""]
+    output_rc_1 = ["""
+ip name-server  10.17.39.11
+"""]
+    output_ping = ["""
+Pinging ntp.inrim.it. (193.204.114.105) with 56 bytes of data:
+
+56 bytes from 193.204.114.105: icmp_seq=1. time=40 ms
+56 bytes from 193.204.114.105: icmp_seq=2. time=20 ms
+56 bytes from 193.204.114.105: icmp_seq=3. time=20 ms
+56 bytes from 193.204.114.105: icmp_seq=4. time=20 ms
+
+----193.204.114.105 PING Statistics----
+4 packets transmitted, 4 packets received, 0% packet loss
+round-trip (ms) min/avg/max = 20/25/40
+"""]
+    output_cfg = ["""
+Polling interval: 60 seconds.
+No MD5 authentication keys.
+Authentication is not required for synchronization.
+No trusted keys.
+"""]
+
+    setup_dut(dut)
+
+    name_server_primary = '10.17.39.11'
+    ntp_host_name = 'ntp.inrim.it'
+
+    adddns_cmd = 'ip name-server {0}'.format(name_server_primary)
+    deldns_cmd = 'no ip name-server {0}'.format(name_server_primary)
+    ping_cmd = 'ping {0}'.format(ntp_host_name)
+    create_cmd = 'sntp server {0}'.format(ntp_host_name)
+    # delete_cmd = 'no sntp server {0}'.format(ntp_host_name)
+    delete_cmd = 'no sntp server 193.204.114.105'
+
+    dut.add_cmd({'cmd': 'show running-config', 'state': 0, 'action': 'PRINT', 'args': output_rc_0})
+    dut.add_cmd({'cmd': adddns_cmd, 'state': 0, 'action': 'SET_STATE', 'args': [1]})
+    dut.add_cmd({'cmd': 'show running-config', 'state': 1, 'action': 'PRINT', 'args': output_rc_1})
+    dut.add_cmd({'cmd': ping_cmd, 'state': 1, 'action': 'PRINT', 'args': output_ping})
+    dut.add_cmd({'cmd': 'show sntp config', 'state': 1, 'action': 'PRINT', 'args': output_cfg})
+    dut.add_cmd({'cmd': 'show sntp status', 'state': 1, 'action': 'PRINT', 'args': output_0})
+    dut.add_cmd({'cmd': create_cmd, 'state': 1, 'action': 'SET_STATE', 'args': [2]})
+    dut.add_cmd({'cmd': 'show sntp config', 'state': 2, 'action': 'PRINT', 'args': output_cfg})
+    dut.add_cmd({'cmd': 'show sntp status', 'state': 2, 'action': 'PRINT', 'args': output_1})
+    dut.add_cmd({'cmd': delete_cmd, 'state': 2, 'action': 'SET_STATE', 'args': [3]})
+    dut.add_cmd({'cmd': 'show sntp config', 'state': 3, 'action': 'PRINT', 'args': output_cfg})
+    dut.add_cmd({'cmd': 'show sntp status', 'state': 3, 'action': 'PRINT', 'args': output_0})
+    dut.add_cmd({'cmd': 'show running-config', 'state': 3, 'action': 'PRINT', 'args': output_rc_1})
+    dut.add_cmd({'cmd': deldns_cmd, 'state': 3, 'action': 'SET_STATE', 'args': [4]})
+    dut.add_cmd({'cmd': 'show running-config', 'state': 4, 'action': 'PRINT', 'args': output_rc_0})
+
+    d = Device(host=dut.host, port=dut.port, protocol=dut.protocol, log_level=log_level, mock=use_mock)
+    d.open()
+    d.dns.create(name_servers=name_server_primary)
+    assert d.dns['name_servers'] == name_server_primary
+    ntp_ip_address = d.dns.read(ntp_host_name)
+    assert '193.204.114.105' == ntp_ip_address
+
+    assert ntp_ip_address not in d.ntp.keys()
+    d.ntp.create(ntp_host_name)
+    assert ntp_ip_address in d.ntp.keys()
+
+    # d.ntp.delete(ntp_host_name)
+    d.ntp.delete(ntp_ip_address)
+    assert ntp_ip_address not in d.ntp.keys()
+
+    d.dns.delete(name_servers=name_server_primary)
+    d.close()
